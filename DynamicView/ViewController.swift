d527bcceb6e654199030c0a5edf50484579001f0
Jason P'ng <jasonpng@jasons-macbook-air.local>
2016-05-04 14:06:38 -0400

    Initial Commit

---
+++ DynamicView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DynamicView
+//
+//  Created by Jason P'ng on 2016-05-04.
+//  Copyright © 2016 Jason P'ng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
